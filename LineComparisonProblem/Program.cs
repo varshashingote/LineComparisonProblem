b067d119c86757c72eba0306d3ca4771d8d1d456
varsha Gunjal <varshashingote01@gmail.com>
2023-01-20 18:44:51 +0530

    [varsha] Line are equal or not

--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -18,8 +18,27 @@ namespace LineComparisonProblem
             Console.WriteLine("Enter the value of x2 ,y2:");
             int x2 = Convert.ToInt32(Console.ReadLine());
             int y2 = Convert.ToInt32(Console.ReadLine());
-            double length = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-            Console.WriteLine("length of line is:" + length);
+            double length1 = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            Console.ReadLine();
+            Console.WriteLine("Enter the values of x3,y3");
+            int x3 = Convert.ToInt32(Console.ReadLine());
+            int y3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of x4 ,y4:");
+            int x4 = Convert.ToInt32(Console.ReadLine());
+            int y4 = Convert.ToInt32(Console.ReadLine());
+            double length2 = Math.Sqrt((x4 - x3) ^ 2 + (y4 - y3) ^ 2);
+            Console.WriteLine("length of line1 is:" + length1);
+            Console.WriteLine("Length of line2 is:"+ length2);
+            if (length1.Equals( length2))
+            {
+                Console.WriteLine("Two lines are equal");
+
+            }
+            else
+            {
+                Console.WriteLine("two lines are not equal");
+            }
+
             Console.ReadLine();
 
         }
