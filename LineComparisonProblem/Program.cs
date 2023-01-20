d13a6a76789f6f0c391954f31620d921c25fd5ff
varsha Gunjal <varshashingote01@gmail.com>
2023-01-20 18:22:41 +0530

    [varsha] Calculate length

--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -12,6 +12,15 @@ namespace LineComparisonProblem
         {
             Console.WriteLine("Welcome to Line Computation Program");
 
+            Console.WriteLine("Enter the value of x1,y1:");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of x2 ,y2:");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            double length = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            Console.WriteLine("length of line is:" + length);
+            Console.ReadLine();
 
         }
     }
